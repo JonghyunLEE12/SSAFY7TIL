6511d0418fa6ca017c986703782f5ce4c031f5a3
이종현 <ijonghyeon@ijonghyeon-ui-MacBookAir.local>
2022-11-27 15:02:05 +0900

    Graph & Tree

--- JS/QueuePractice.js
+++ JS/QueuePractice.js
@@ -1,10 +1,10 @@
 arr = [1,2,3,4,5,6]
 // const sum = arr.reduce((a,b) => a+b , 0)
 
-console.log(Math.max(...arr))
+// console.log(Math.max(...arr))
 
-const sum = arr.reduce((a,b) => a+b,0)
-console.log(sum)
+// const sum = arr.reduce((a,b) => a+b,0)
+// console.log(sum)
 
 
 // class Queue {
@@ -61,26 +61,30 @@ console.log(sum)
 
 
 class Queue {
-    constructor(){
-        this.queue = []
-        this.front = 0
-        this.rear = 0
-    }
+    // constructor(){
+    //     this.queue = []
+    //     this.front = 0
+    //     this.rear = 0
+    // }
+
+    #queue = []
+    #front = 0
+    #rear = 0
     enqueue(value) {
-        this.queue[this.rear++] = value
+        this.#queue[this.#rear++] = value
     }
 
     dequeue(){
-        const value = this.queue[this.front]
-        delete this.queue[this.front]
-        this.front += 1
+        const value = this.#queue[this.#front]
+        delete this.#queue[this.#front]
+        this.#front += 1
         return value
     }
     peek(){
-        return this.queue[this.front]
+        return this.#queue[this.#front]
     }
     size(){
-        return this.rear - this.front
+        return this.#rear - this.#front
     }
 }
 
@@ -92,8 +96,6 @@ console.log(queue.size())
 console.log(queue.peek())
 queue.enqueue(2)
 queue.enqueue(3)
-console.log(queue)
-console.log(queue)
 console.log(queue.size())
 console.log(queue.peek())
 
